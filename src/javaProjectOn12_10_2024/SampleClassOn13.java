d1019e57125cf61f46506829f0e25a8352176ddc
srihari <srihariadada19978@gmail.com>
2024-10-13 16:03:50 +0530

    added code for sampleclassOn13 y indian employees

--- src/javaProjectOn12_10_2024/SampleClassOn13.java
+++ src/javaProjectOn12_10_2024/SampleClassOn13.java
@@ -1,5 +1,10 @@
 package javaProjectOn12_10_2024;
 
 public class SampleClassOn13 {
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+	}
 
 }
